890f85e222b4f56b173a75cc6c9cb0637ec602fe
Rafael Sousa (Rafao) <rafael.sousa@resource.com.br>
2017-04-19 15:23:47 -0300

    commit

--- app/src/main/java/com/example/rafaelsousa/storageaccessframeworkexample/MainActivity.java
+++ app/src/main/java/com/example/rafaelsousa/storageaccessframeworkexample/MainActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,8 +31,18 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == READ_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
             final Uri uri = data.getData();
             Log.d("MainActivity", "Uri: " + uri.toString());
-            File auxFile = new File(uri.toString());
-            Toast.makeText(this, auxFile.getName(), Toast.LENGTH_LONG).show();
+            byte[] fileByteArray = new byte[(int) uri.toString().length()];
+
+            try {
+                FileInputStream fileInputStream =
+                        (FileInputStream) getContentResolver().openInputStream(data.getData());
+                fileInputStream.read(fileByteArray);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            String encodedFile = Base64.encodeToString(fileByteArray, Base64.NO_WRAP);
+            Toast.makeText(this, encodedFile, Toast.LENGTH_LONG).show();
         }
     }
 }
